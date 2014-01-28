1a3039f05d24c7a3fc435f44f08206910b48ee34
phaniram <maturusrinivasphaniram@gmail.com>
2014-01-28 09:33:07 -0500

    Initial Version

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ amulyam.in-Worker
 =================
 
 Workers to do specific tasks on amulyam.in
+    --dailyLogin.js : Will login to Amulyam daily and will logout.
---
+++ dailyLogin.js
@@ -0,0 +1,55 @@
+var http = require('http');
+(function () {
+  "use strict";
+    var request = require('request');
+    var cheerio = require('cheerio');
+    request = request.defaults({jar: true});
+    var interval = 24 * 60 * 60 * 1000;
+    
+    var AMULYAM_USERNAME = process.env.AMULYAM_USERNAME;
+    var AMULYAM_PASSWORD = process.env.AMULYAM_PASSWORD;
+    var AMULYAM_LOGIN_URL = 'http://www.amulyam.in/login.do';
+    var AMULYAM_HOME_URL = 'http://www.amulyam.in/';
+    var AMULYAM_LOGOUT_URL = 'http://www.amulyam.in/logout.do';
+
+    var dailyLoginWorker = function(){
+        console.log("Trying to login now");
+        request.post(AMULYAM_LOGIN_URL,{ form: { mobile: AMULYAM_USERNAME ,   password: AMULYAM_PASSWORD}},
+            function (error, response, html) {
+                if (!error && response.statusCode == 200) {
+                    console.log("Login SUCCESS");
+                    request(AMULYAM_HOME_URL, function (err, resp, body) {
+                      if (!err && resp.statusCode == 200) {
+                        console.log("Gone to Home Page ");
+                        var $ = cheerio.load(body);
+                        $('span.balance').each(function(i, element){
+                          console.log("Balance : "+$(this).text());
+                        });
+                        request(AMULYAM_LOGOUT_URL, function (logout_error, logout_resp, logout_html) {
+                            if (!logout_error && logout_resp.statusCode == 200) {
+                                console.log("Logged Out");
+                            } else
+                              {
+                                console.log("Error navigating to "+AMULYAM_LOGOUT_URL+"  :   "+JSON.stringify(logout_error)); 
+                              }
+                           });
+                      }else
+                      {
+                         console.log("Error navigating to "+AMULYAM_HOME_URL+"  :   "+JSON.stringify(err)); 
+                      }
+                    });
+                } else
+              {
+                console.log("Error navigating to "+AMULYAM_LOGIN_URL+"  :   "+JSON.stringify(error)); 
+              }
+            }
+        );
+    };
+  dailyLoginWorker();
+  if (interval > 0) setInterval(dailyLoginWorker, interval);
+}) ();
+var port = Number(process.env.PORT || 5000);
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from Amulayam Daily Login Worker \n');
+}).listen(port);
